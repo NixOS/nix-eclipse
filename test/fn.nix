f0ecdf8d2e18e921101c9c937c109fc2c9af1d27
Rob Vermaas <rob.vermaas@gmail.com>
2010-07-28 09:32:31 +0000

    nix eclipse editor: add reference resolving for local attribute/let/function binds

--- test/fn.nix
+++ test/fn.nix
@@ -1,11 +1,11 @@
-/*let 
-  add = a : b : a+b ;
-in 
-  add 2 3 "asd asd"
+let a = 1;
+in
+
+{
+  c = a: a;
+  d = {b}:b;  
+  b =1 ;  
   
-  */
-  let a = 1; in
-    {fontconfig ? fontconfig_, fontDirectories}:
-    import ../development/libraries/fontconfig/make-fonts-conf.nix {
-      inherit runCommand libxslt fontconfig fontDirectories;
-    }
+  inherit a;
+  inherit (pkgs) a;
+}
